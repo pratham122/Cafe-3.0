a4473452cde1a83e864d5704c50c2aaef4904519
Prathamraj <pratham6122@gmail.com>
2023-08-14 10:48:46 +0530

    Some changes

--- src/components/careInner.js
+++ src/components/careInner.js
@@ -3,7 +3,7 @@ import React from "react";
 function careInner(props) {
     return (
         <div className="carousel-item active">
-            <img src={require('{props.careImg}')} alt="Carousel-img-1" className="d-block w-100" />
+            <img src={props.careImg} alt="Carousel-img-1" className="d-block w-100" />
         </div>
     );
 }
