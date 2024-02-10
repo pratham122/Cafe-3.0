14a65e4977d8730afd57bca43b2a0a3ece4467fb
manasishewale <manasishewale9@gmail.com>
2024-02-10 11:42:15 +0530

    cart updated

--- backend/Routes/CreateUser.js
+++ backend/Routes/CreateUser.js
@@ -3,17 +3,26 @@ const router = express.Router()
 const User = require('../models/Users')
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
-const jwtSecret = "MynameisEndtoEndYouTubeChannelgvsjcjhb1#"
-const { body, validationResult } = require('express-validator')
+const jwtSecret = "Thisisamernstackprojectgvsjcjhb1#"
+const {
+    body,
+    validationResult
+} = require('express-validator')
 router.post("/createuser", [body('email').isEmail(),
-        body('name').isLength({ min: 5 }),
-        body('password').isLength({ min: 5 })
+        body('name').isLength({
+            min: 5
+        }),
+        body('password').isLength({
+            min: 5
+        })
     ],
     async(req, res) => {
 
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
-            return res.status(400).json({ errors: errors.array() });
+            return res.status(400).json({
+                errors: errors.array()
+            });
         }
         const salt = await bcrypt.genSalt(10);
         let secPassword = await bcrypt.hash(req.body.password, salt)
@@ -24,31 +33,45 @@ router.post("/createuser", [body('email').isEmail(),
                 email: req.body.email,
                 location: req.body.location
             })
-            res.json({ success: true });
+            res.json({
+                success: true
+            });
         } catch (error) {
             console.log(error)
-            res.json({ success: false });
+            res.json({
+                success: false
+            });
         }
     })
 
 router.post("/loginuser", [body('email').isEmail(),
-        body('password').isLength({ min: 5 })
+        body('password').isLength({
+            min: 5
+        })
     ],
     async(req, res) => {
         let email = req.body.email;
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
-            return res.status(400).json({ errors: errors.array() });
+            return res.status(400).json({
+                errors: errors.array()
+            });
         }
         try {
-            let userData = await User.findOne({ email });
+            let userData = await User.findOne({
+                email
+            });
             if (!userData) {
-                return res.status(400).json({ errors: "Try with correct password" });
+                return res.status(400).json({
+                    errors: "Try with correct password"
+                });
             }
 
             const pwdCompare = await bcrypt.compare(req.body.password, userData.password)
             if (!pwdCompare) {
-                return res.status(400).json({ errors: "Try with correct password" });
+                return res.status(400).json({
+                    errors: "Try with correct password"
+                });
             }
 
             const data = {
@@ -57,10 +80,15 @@ router.post("/loginuser", [body('email').isEmail(),
                 }
             }
             const authToken = jwt.sign(data, jwtSecret)
-            return res.json({ success: true, uathToken: authToken });
+            return res.json({
+                success: true,
+                authToken: authToken
+            });
         } catch (error) {
             console.log(error)
-            res.json({ success: false });
+            res.json({
+                success: false
+            });
         }
     })
 
--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,85 +1,95 @@
 import React, { useState } from "react";
-import {useDispatchCart } from "./ContextReducer";
+import { useDispatchCart } from "./ContextReducer";
 
 function Cart(props) {
-    let dispatch=useDispatchCart();
-    let index=props.index;
-    const [count, setCount] = useState(props.qty);
-    const [added, setAdded] = useState(false);
-    function handleAddClick() {
-        if (!added) {
-            setAdded(true); // Mark item as added
-        }
-        setCount(count + 1);
+  let dispatch = useDispatchCart();
+  let index = props.index;
+  const [count, setCount] = useState(props.qty);
+  const [added, setAdded] = useState(false);
+  function handleAddClick() {
+    if (!added) {
+      setAdded(true); // Mark item as added
     }
- 
-    function handleDecreaseClick() {
-        if (count === 1) {
-            setAdded(false);
-        }
-        setCount(count - 1);
+    setCount(count + 1);
+  }
+
+  function handleDecreaseClick() {
+    if (count === 1) {
+      setAdded(false);
     }
-    return (
-        <div className="card-body">
-                                {/* <!-- Single item --> */}
-                                <div className="row">
-                                    <div className="col-lg-3 col-md-12 mb-4 mb-lg-0">
-                                        {/* <!-- Image --> */}
-                                        <div className="bg-image hover-overlay hover-zoom ripple rounded" data-mdb-ripple-color="light">
-                                            <img src={props.img}
-                                                className="w-100" alt="" />
+    setCount(count - 1);
+  }
+  return (
+    <div className="card-body">
+      {/* <!-- Single item --> */}
+      <div className="row">
+        <div className="col-lg-3 col-md-12 mb-4 mb-lg-0">
+          {/* <!-- Image --> */}
+          <div
+            className="bg-image hover-overlay hover-zoom ripple rounded"
+            data-mdb-ripple-color="light"
+          >
+            <img src={props.img} className="w-100" alt="" />
+          </div>
+          {/* <!-- Image --> */}
+        </div>
 
-                                        </div>
-                                        {/* <!-- Image --> */}
-                                    </div>
-
-                                    <div className="col-lg-5 col-md-6 mb-4 mb-lg-0">
-                                        {/* <!-- Data --> */}
-                                        <p><strong>{props.name}</strong></p>
-                                        <button type="button" className="btn btn-danger btn-sm me-1 mb-2" data-mdb-toggle="tooltip"
-                                            title="Remove item" onClick={()=>{dispatch({type:"REMOVE",index:index})}}>
-                                            <i className="fas fa-trash"></i>
-                                        </button>
-                                        {/* <button type="button" className="btn btn-danger btn-sm mb-2" data-mdb-toggle="tooltip"
+        <div className="col-lg-5 col-md-6 mb-4 mb-lg-0">
+          {/* <!-- Data --> */}
+          <p>
+            <strong>{props.name}</strong>
+          </p>
+          <button
+            type="button"
+            className="btn btn-danger btn-sm me-1 mb-2"
+            data-mdb-toggle="tooltip"
+            title="Remove item"
+            onClick={() => {
+              dispatch({ type: "REMOVE", index: index });
+            }}
+          >
+            <i className="fas fa-trash"></i>
+          </button>
+          {/* <button type="button" className="btn btn-danger btn-sm mb-2" data-mdb-toggle="tooltip"
                                             title="Move to the wish list">
                                             <i className="fas fa-heart"></i>
                                         </button> */}
-                                        {/* <!-- Data --> */}
-                                    </div>
+          {/* <!-- Data --> */}
+        </div>
 
-                                    <div className="col-lg-4 col-md-6 mb-4 mb-lg-0">
-                                        {/* <!-- Quantity --> */}
-                                        <div className="d-flex mb-4" style={{ "max-width": "300px" }}>
+        <div className="col-lg-4 col-md-6 mb-4 mb-lg-0">
+          {/* <!-- Quantity --> */}
+          <div className="d-flex mb-4" style={{ "max-width": "300px" }}>
+            <div className="col-lg-10 col-md-8 col-sm-6 add-buttons">
+              <button
+                onClick={handleAddClick}
+                className="btn btn-outline-success qty-btnPlus"
+              >
+                +
+              </button>
+              <p className="qty-count">{count}</p>
+              <button
+                onClick={handleDecreaseClick}
+                className="btn btn-outline-success qty-btnMinus"
+              >
+                -
+              </button>
+            </div>
+          </div>
+          {/* <!-- Quantity --> */}
 
+          {/* <!-- Price --> */}
+          <p className="text-start text-md-center">
+            <strong>{props.price}</strong>
+          </p>
+          {/* <!-- Price --> */}
+        </div>
+      </div>
+      {/* <!-- Single item --> */}
 
-                                            
-                                                <div className="col-lg-10 col-md-8 col-sm-6 add-buttons">
-                                                    <button onClick={handleAddClick} className="btn btn-outline-success qty-btnPlus">+</button>
-                                                    <p className="qty-count">{count}</p>
-                                                    <button onClick={handleDecreaseClick} className="btn btn-outline-success qty-btnMinus">-</button>
-                                                </div>
-                                            
-
-
-                                        </div>
-                                        {/* <!-- Quantity --> */}
-
-                                        {/* <!-- Price --> */}
-                                        <p className="text-start text-md-center">
-                                            <strong>{props.price}</strong>
-                                        </p>
-                                        {/* <!-- Price --> */}
-                                    </div>
-                                </div>
-                                {/* <!-- Single item --> */}
-
-                                <hr className="my-4" />
-
-
-                            </div>
-
-
-    );
+      <hr className="my-4" />
+    </div>
+  );
 }
 
 export default Cart;
